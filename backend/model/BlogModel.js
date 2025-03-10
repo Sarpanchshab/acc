17bff9d38e4af4a255bc358d63d78ffc89e58951
jhaharishankar <harishankarjha21@gmail.com>
2025-03-10 16:56:36 +0530

    BlogModel Change

--- backend/model/BlogModel.js
+++ backend/model/BlogModel.js
@@ -1,15 +1,11 @@
 const mongoose = require("mongoose");
 
 const BlogSchema = new mongoose.Schema({
-    // email: {
-    //     type: String,
-    //     required: true,
-    //     unique: true
-    // },
-    // password: {
-    //     type: String,
-    //     required: true,
-    // },
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    }
 }, { timestamps: true });
 
 const BlogModel = mongoose.model("BlogModel", BlogSchema);
