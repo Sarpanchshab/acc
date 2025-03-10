6492f3cc9eb71408b2544a4d88c0794c805ee497
Anuragkushwah21 <anuragkushwah5100@gmail.com>
2025-03-10 17:33:17 +0530

    Course updated backend

--- backend/model/Course.js
+++ backend/model/Course.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 
 const CourseSchema = new mongoose.Schema({
-    // email: {
-    //     type: String,
-    //     required: true,
-    //     unique: true
-    // },
-    // password: {
-    //     type: String,
-    //     required: true,
-    // },
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    duration: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: String,
+        required: true,
+    },
+    
 }, { timestamps: true });
 
 const CourseModel = mongoose.model("CourseModel", CourseSchema);
