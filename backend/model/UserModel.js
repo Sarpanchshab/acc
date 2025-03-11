828f9649e7e37db29be2cbc4d92053aed4fd5aba
Sarpanchshab <monu1999june@gmail.com>
2025-03-11 21:17:06 +0530

    Merge Conflict 2

--- backend/model/UserModel.js
+++ backend/model/UserModel.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-<<<<<<< HEAD
-    name:{
-=======
     name: {
->>>>>>> 31ab57070a5221a760d3cf0e1611c013b491fa11
         type: String,
         required: true,
     },
